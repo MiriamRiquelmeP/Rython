57d11cb72693c5b3445437bf0c8d5d57a1bfbac2
fpsanz <fernando.perez8@um.es>
2020-06-10 14:55:47 +0200

    ya ta

--- app.R
+++ app.R
@@ -1,4 +1,3 @@
-
 library(shiny)
 library(shinydashboard)
 library(shinyalert)
@@ -13,6 +12,8 @@ library(ShinyImage)
 library(EBImage)
 library(DiagrammeR)
 library(png)
+#devtools::install_github('wleepang/shiny-directory-input')
+library(shinyDirectoryInput)
 source("utils.R")
 
 reticulate::use_virtualenv("~/Rython/")
@@ -43,11 +44,11 @@ sidebar <- dashboardSidebar(useShinyalert(),
                                   options = list(title = "Option ..."),
                                   selected = NULL ) )
                             ),
-                            sidebarMenu( 
-                              menuItem(
-                                fluidRow( column(12, align = "center", offset=0,
-                                                 uiOutput("loadftv"),
-                                                 uiOutput("loadfitmodel") ) ) ) ),
+                            # sidebarMenu( 
+                            #   menuItem(
+                            #     fluidRow( column(12, align = "center", offset=0,
+                            #                      #uiOutput("loadftv"),
+                            #                      uiOutput("loadfitmodel") ) ) ) ),
                             sidebarMenu(
                               menuItem(
                                 fluidRow(column(12, align = "center", offset=0,
@@ -68,18 +69,24 @@ sidebar <- dashboardSidebar(useShinyalert(),
                                 )
                               )
                             ),
+                            tags$hr(style="border-color: white; margin-left: 10px; margin-right: 10px"),
+                             sidebarMenu(
+                               menuItem(
+                                 fluidRow(column(12, align = "center", offset = 0,
+                                                 uiOutput("Model")
+                                                 ) ) ) ),
                             sidebarMenu(
                               menuItem(
                                 fluidRow(column(12, align = "center", offset = 0,
-                                                uiOutput("savefvt"),
+                                                uiOutput("fileName"),
                                                 uiOutput("savefitmodel") ) ) ) )
                             )
 
 ### BODY ###############
 body <- dashboardBody(
-      tags$head(
-        tags$link(rel = "stylesheet", type = "text/css", href = "customDark.css")
-      ),
+      # tags$head(
+      #   tags$link(rel = "stylesheet", type = "text/css", href = "customDark.css")
+      # ),
       setShadow(class = "shiny-plot-output"),
       setShadow( class = "box"),
       setShadow( class = "svg-container"),
@@ -96,15 +103,15 @@ body <- dashboardBody(
       bsAlert("alert"),
       fluidPage( fluidRow( 
         column(width = 3,
-               grVizOutput("dg", height = "600px"),
-               sliderInput("thres", "Select threshold", min = 0, max = 5000, value=2000, step=10) ),
+               sliderInput("thres", "Select threshold", min = 0, max = 5000, value=2000, step=10, width="100%") ,
+               grVizOutput("dg", height = "600px")),
       column(width=9, 
              #plotOutput("imagen", click = "plot_click"),
              uiOutput("image"),
-             textOutput("texto")
-      )),
-      fluidRow(column(width = 9, offset = 3,
-                      textOutput("resultxt")))
+             textOutput("texto"),
+             tags$br(),
+             htmlOutput("resultxt")
+      ))
       )
 )
      
@@ -135,12 +142,58 @@ server <- function(input, output, session) {
                imageUrl = "dna-svg-small-13.gif", 
                imageWidth = 200, imageHeight = 100)})
   
-  # mostrar flujograma ##
-  # output$dg <- renderGrViz({
-  #   # TODO meter todo lo que falta aqui
-  # })
-  # 
-  # 
+shinyImageFile <- reactiveValues(shiny_img_origin = NULL)
+va <- reactiveValues(x=NULL, y=NULL)
+nova <- reactiveValues(x=NULL, y=NULL)
+counter <- 1
+train <- reactiveValues(ok=FALSE)
+fv <- reactiveValues(ok = FALSE)
+loadimage <- reactiveValues(ok = FALSE)
+manualTrain <- reactiveValues(ok = FALSE)
+selectAlgorithm <- reactiveValues(ok = FALSE)
+
+# mostrar flujograma #####
+output$dg <- renderGrViz({
+  if(isTRUE(loadimage$ok)){ col1="'#3c8dbc'"; fcol1="'#ffffff'" }
+  if(isTRUE(manualTrain$ok)){col2="'#3c8dbc'"; fcol2="'#ffffff'"}
+  if(isTRUE(fv$ok)){col6="'#3c8dbc'"; fcol6="'#ffffff'";col7="'#3c8dbc'"; fcol7="'#ffffff'" }
+  if(isTRUE(tr$ok)){col8="'#3c8dbc'"; fcol8="'#ffffff'"; col9="'#3c8dbc'"; fcol9="'#ffffff'"}
+  if(isTRUE(clf$ok)){col10="'#3c8dbc'"; fcol10="'#ffffff'"}
+  if(isTRUE(stat$ok)){col11="'#3c8dbc'"; fcol11="'#ffffff'"}
+  if(input$option=="opt2"){col3="'#3c8dbc'"; fcol3="'#ffffff'"}
+  if(isTRUE(cargarmodelo$ok)){col5="'#3c8dbc'"; fcol5="'#ffffff'"}
+qq <- paste0(
+  "[1]: ",col1,
+  "\n[2]: ",col2,
+  "\n[3]: ",col3,
+  "\n[4]: ",col4,
+  "\n[5]: ",col5,
+  "\n[6]: ",col6,
+  "\n[7]: ",col7,
+  "\n[8]: ",col8,
+  "\n[9]: ",col9,
+  "\n[10]: ",col10,
+  "\n[11]: ",col11,
+  "\n[12]: ",col12,
+  "\n[13]: ",col13,
+  "\n[14]: ",fcol1,
+  "\n[15]: ",fcol2,
+  "\n[16]: ",fcol3,
+  "\n[17]: ",fcol4,
+  "\n[18]: ",fcol5,
+  "\n[19]: ",fcol6,
+  "\n[20]: ",fcol7,
+  "\n[21]: ",fcol8,
+  "\n[22]: ",fcol9,
+  "\n[23]: ",fcol10,
+  "\n[24]: ",fcol11,
+  "\n[25]: ",fcol12,
+  "\n[26]: ",fcol13
+)
+k <- c(kk, qq )  
+grViz(k, engine = "dot")
+   })
+
 # InputFile #################
 output$Image <- renderUI({
   fileInput("imagenFile",
@@ -149,21 +202,17 @@ output$Image <- renderUI({
             accept = c(".png",".jpg") )
 })
 
+# render Contenedor de imagen ################
 output$image <- renderUI({
   if(input$option == "opt1"){
-    plotOutput("imagen", click = "plot_click")
+    manualTrain$ok <- TRUE
+    shinyalert("Mark points into vacuole object")
+    plotOutput("imagen", click = "plot_click", height = "685px")
   } else{
-    plotOutput("imagen")
+    manualTrain$ok <- FALSE
+    plotOutput("imagen", height = "685px")
   }
-    
 })
-
-shinyImageFile <- reactiveValues(shiny_img_origin = NULL)
-va <- reactiveValues(x=NULL, y=NULL)
-nova <- reactiveValues(x=NULL, y=NULL)
-counter <- 1
-train <- reactiveValues(ok=FALSE)
-fv <- reactiveValues(ok = FALSE)
 
 renameUpload <- function(inFile) {
     if(is.null(inFile))
@@ -174,7 +223,7 @@ renameUpload <- function(inFile) {
     inFile$datapath <- newNames
     return(inFile$datapath)
   }
-    
+
 observeEvent(input$option, {
     validate(need(!is.null(input$imagenFile),""))
     #validate(need(input$option,""))
@@ -183,8 +232,48 @@ observeEvent(input$option, {
     cargarImagen(imagenNew, imagenNew$image)
     shinyImageFile$shiny_img_origin <- shinyimg$new(renameUpload(input$imagenFile))
     output$imagen <- renderPlot({shinyImageFile$shiny_img_origin$render()})
-    shinyalert("Mark points into vacuole object")
-  })        
+    loadimage$ok <- TRUE
+  })   
+
+# salvar modelo ############################
+output$savefitmodel <- renderUI({
+  validate(need(isTRUE(tr$ok),""))
+  actionButton("saveModel", label = "Save model")
+})
+output$fileName <- renderUI({
+  validate(need(isTRUE(tr$ok),""))
+  textInput("filename", "Model name to save")
+})
+
+observeEvent(input$saveModel,{
+  validate(need(!is.null(input$filename),"") )
+  path <- choose.dir(default = "/")
+  file <- paste0(path,"/",input$filename)
+  saveModel(imagenNew, file)
+})
+######    ######       #####
+
+# cargar modelo ###########################
+output$Model <- renderUI({
+  validate(need(input$option=="opt2", ""))
+   fileInput("filemodel",
+             "Select model file",
+             placeholder = "model")
+ })
+
+cargarmodelo <- reactiveValues(ok=FALSE)
+observeEvent(input$filemodel, {
+  filepath <- input$filemodel
+  if(is.null(filepath)){return(NULL)}
+  imagenNew$clf = loadModel(filepath$datapath)
+  cargarmodelo$ok <- TRUE #
+  B = classImage(imagenNew)
+  imagenNew$B = B
+  clf$ok <- TRUE #
+  BR <- py_2_R_imageBW(B)
+  output$imagen <- renderPlot({display(BR, method = "raster")})
+  
+})
 
 # marcar puntos de entrenamiento manual ##############
 observeEvent(input$plot_click,{
@@ -214,7 +303,7 @@ output$texto <- renderText({
                "; vac: ",length(va$x),
                "; novac: ",length(nova$x) )
     })
-
+# feature vector ###################
 observeEvent(train$ok,{
   if( isTRUE(train$ok) ){
     vac <- featureCoord(list(x = va$x, y=va$y ) )
@@ -228,7 +317,7 @@ observeEvent(train$ok,{
     fv$ok <- TRUE
   }
 })
-# 
+# select algorithm ######################
 output$algorithm <- renderUI({
   validate(need(isTRUE(fv$ok), ""))
     pickerInput(
@@ -244,7 +333,7 @@ output$algorithm <- renderUI({
 })
 
 tr <- reactiveValues(ok=FALSE)
-
+# train model ###############################
 observeEvent(input$algoritmo,{
   validate(need(input$algoritmo,""))
   if( input$algoritmo == "knn"){
@@ -253,13 +342,14 @@ observeEvent(input$algoritmo,{
   }
 })
 
+# render boton class #######################
 output$botonClass <- renderUI({
   validate(need(isTRUE(tr$ok),""))
   actionButton(inputId = "buttonClass", label="Classify")
 })
 
 clf <- reactiveValues(ok=FALSE)
-
+# clasifica imagen ######################
 observeEvent(input$buttonClass, {
   if(isTRUE(tr$ok)){
     B = classImage(imagenNew)
@@ -283,7 +373,7 @@ observeEvent(input$buttonResults,{
 })
 
 observeEvent(input$thres,{
-  validate(need(input$thres,""))
+  #validate(need(input$thres,""))
   validate(need(isTRUE(stat$ok),"" ) )
   thr = input$thres # se pasará por según valor deslizador
   threshold(imagenNew, thr)
@@ -293,23 +383,16 @@ observeEvent(input$thres,{
   output$imagen <- renderPlot({display(maskR, method = "raster")}) 
 })
 
+output$resultxt <- renderUI({
+  validate(need(isTRUE(stat$ok),"" ) )
+  validate(need(input$thres,"" ) )
+  texto <- paste(paste0("Mean big size: ",imagenNew$bigMean),
+                 paste0("Total area: ",imagenNew$totArea),
+                 paste0("Percent area big: ",imagenNew$bigPercent), sep ="<br/>")
+  HTML(texto)
+})
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # Run the application 
--- utils.R
+++ utils.R
@@ -29,3 +29,84 @@ featureCoord <- function(data, type){
     return(listvac)
 }
 
+
+kk <- "
+digraph a_nice_graph {
+
+graph [splines=polyline, bgcolor='#ecf0f5', ratio=1.5]
+
+#node color
+node[fillcolor='@@1', fontcolor='@@14', style=filled, label='Load Image', fontsize=9, shape=oval]
+a
+node[fillcolor='@@2', fontcolor='@@15', style=filled, label='Manual train']
+b
+node[fillcolor='@@3', fontcolor='@@16', style=filled, label='Train from model']
+c
+#node[fillcolor='@@4', fontcolor='@@17', style=filled, label='Load feature vector']
+#d
+node[fillcolor='@@5', fontcolor='@@18', style=filled, label='Load fitted model']
+e
+node[fillcolor='@@6', fontcolor='@@19', style='filled,dashed', label='Mark points']
+f
+node[fillcolor='@@7', fontcolor='@@20', style='filled,dashed', label='Generate feature vector']
+g
+node[fillcolor='@@8', fontcolor='@@21', style=filled, label='Select algorithm']
+h
+node[fillcolor='@@9', fontcolor='@@22', style='filled,dashed', label='Train model']
+i
+node[fillcolor='@@10', fontcolor='@@23', style='filled,dashed', label='Classify image']
+j
+node[fillcolor='@@11', fontcolor='@@24', style=filled, label='View results']
+k
+#node[fillcolor='@@12', fontcolor='@@25', style=filled, label='Save feature vector', shape=record]
+#l
+node[fillcolor='@@13', fontcolor='@@26', style=filled, label='Save model', shape=record]
+m
+
+# edge definitions with the node IDs
+a -> {b,c}
+c -> {e}
+b -> f
+f -> g
+g -> h
+h -> i
+i -> j
+j -> k
+#g -> l [style=dashed, color=grey]
+i -> m [style=dashed, color=grey]
+#d -> h
+e -> j
+{rank=same; b;c}
+#{rank=same; d;e}
+#{rank=same; l;g} 
+#{rank=same; m;i}
+}
+"
+col1 <- "'#ecf0f5'"
+col2 <- "'#ecf0f5'"
+col3 <- "'#ecf0f5'"
+col4 <- "'#ecf0f5'"
+col5 <- "'#ecf0f5'"
+col6 <- "'#ecf0f5'"
+col7 <- "'#ecf0f5'"
+col8 <- "'#ecf0f5'"
+col9 <- "'#ecf0f5'"
+col10 <- "'#ecf0f5'"
+col11 <- "'#ecf0f5'"
+col12 <- "'#ecf0f5'"
+col13 <- "'#ecf0f5'"
+
+fcol1 <- "'#222d32'"
+fcol2 <- "'#222d32'"
+fcol3 <- "'#222d32'"
+fcol4 <- "'#222d32'"
+fcol5 <- "'#222d32'"
+fcol6 <- "'#222d32'"
+fcol7 <- "'#222d32'"
+fcol8 <- "'#222d32'"
+fcol9 <- "'#222d32'"
+fcol10 <- "'#222d32'"
+fcol11 <- "'#222d32'"
+fcol12 <- "'#222d32'"
+fcol13 <- "'#222d32'"
+
